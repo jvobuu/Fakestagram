d8747ba15d4d93819c95d06ecebf0838b2d83798
Jeremy-Vo-Buu <vobuujeremy@hotmail.com>
2022-07-11 18:15:12 -0400

    created react app

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Fakestagram</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>This is Fakestagram</h1>
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  )
+}
+
+export default Home
