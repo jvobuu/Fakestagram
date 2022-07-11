c17cc69f837cc7eaa72ac3575f6ea1aebc16c8d8
Jeremy-Vo-Buu <vobuujeremy@hotmail.com>
2022-07-11 19:02:49 -0400

    header logo and header search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from "../components/Header"
 
 const Home: NextPage = () => {
   return (
@@ -10,9 +11,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>This is Fakestagram</h1>
       {/* Header */}
-
+      <Header />
       {/* Feed */}
 
       {/* Modal */}
